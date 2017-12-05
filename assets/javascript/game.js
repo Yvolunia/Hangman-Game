75d3f4eb96b9cfc4c497bb1f856291c43723a700
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2017-12-05 08:37:31 -0500

    Game Updates 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,15 @@
-game.js
+// When the facButton is clicked...
+$("#factButton").on("click", function() {
+	// We generate a random number
+	var number = Math.floor((Math.random() * computerChoices.length));
+	// We display the fact from the booFactArray that is in the random position we just generated
+	$("#factText").text(computerChoices[number])
+})
 
-<script type="text/javascript">
-    $(document).ready(function() {
-      // CREATE THE MISSING CODE HERE. Your code should add content to the random-number div.
-    
-  $("#random-button").on("click", function() {
-  var lottoNumber = "";
-  var random = Math.floor(Math.random() * 10);
-  $("#random-number").text(random);
+// Vocab Array
+var computerChoices = ["clairvoyant", "aesthetic", "amicable", "asylum", "benevolent", "camaraderie",
+"circuitous", "demagogue", "empathy", "ephemeral", "fortuitous", "hackneyed"; "hypothesis", "impetuous",
+"integrity", "nonochalant", "opulent", "ostentatious", "procrastinate", "querulous", "resilient", "spontaneity",
+"sagacity"]
 
-  for (var i = 0; i < 9; i++) {
-    var random = (Math.floor(Math.random() * 10);
-      lottoNumber = random + lottoNumber;
-    }
-    
-    var newDiv = $("<div>");
-    newDiv.text = (lottoNumber);
-    $("random-number").prepend(newDiv);
 
-      })  
-
-      } 
-    
-    });
-  </script>
